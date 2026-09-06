396a3a103e4e2e6ae4ec2ebf4ad7d89f0b07ba93
Fillos <128302952+Fillos-A98@users.noreply.github.com>
2026-09-06 15:41:18 +0300

    Replace awww with mpvpaper

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -52,12 +52,11 @@ local menu        = "hyprlauncher"
 hl.on("hyprland.start", function()
 --    hl.exec_cmd("nwg-dock-hyprland -d -hd 50 -l overlay -i 30 -a center -lp start -mb 20 -nolauncher")
     hl.exec_cmd("waybar")
-    hl.exec_cmd("awww-daemon")
+    hl.exec_cmd("mpvpaper -o 'no-audio --loop-file=inf --panscan=1' '*' /usr/share/sddm/themes/sddm-astronaut-theme/Backgrounds/hyprland_kath.mp4")
     hl.exec_cmd("swaync")
     hl.exec_cmd("swayosd-server")
     hl.exec_cmd("wl-paste --type text --watch cliphist store")
     hl.exec_cmd("~/.cache/yay/hyprwave/hyprwave-1.0/hyprwave")
-    hl.dsp.exec_cmd("awww img /home/fillos/.config/wallpaper.jpg")
     hl.exec_cmd("wl-paste --type image --watch cliphist store")
 end)
 
